820de570c36651e168cc7068f34066b9ee02fa76
Sanjib <sanjib12sinha@gmail.com>
2021-09-22 17:36:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+A new Flutter project for Developing Flutter App from Scratch
+
+- [Lab: Read the Latest Updated Articles on Flutter Tool Kit](https://sanjibsinha.com/category/flutter)
+
+## Get the necessary Guide
+
+- [Beginning Flutter with Dart](https://leanpub.com/beginningflutterwithdart)
+- [Better Flutter](https://leanpub.com/betterflutter)
+- [State in Flutter](https://leanpub.com/stateinflutter)
+- [A Complete Flutter Guide: From Beginners to Advanced](https://leanpub.com/b/acompleteflutterguidefrombeginnerstoadvanced)
+
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material Design'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: null,
+                child: const Text(
+                  'Text Button',
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Text Button',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
