ded70fd34806524c17ca6094706aaed077ee3d34
Sanjib <sanjib12sinha@gmail.com>
2021-09-25 10:07:43 +0530

    drawer added

---
+++ lib/drawer_example/drawer_example.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class DrawerExample extends StatelessWidget {
+  const DrawerExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: '',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('')),
+      body: const Center(
+          child: Text(
+        'A drawer is an invisible side screen.',
+        style: TextStyle(fontSize: 20.0),
+      )),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("Sanjib Sinha"),
+              accountEmail: Text("sanjib@sanjibsinha.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.orange,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text("Settings"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.contacts),
+              title: const Text("Contact Us"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'bottom_navigation_bar/models/select_index.dart';
-import 'bottom_navigation_bar/views/bottom_navigation_bar.dart';
+import 'drawer_example/drawer_example.dart';
+//import 'bottom_navigation_bar/views/bottom_navigation_bar.dart';
 
 //import 'bottom_navigation_bar/bottom_navigation_bar.dart';
 
@@ -16,7 +17,8 @@ void main() {
           create: (_) => SelectIndex(),
         ),
       ],
-      child: const BottomNavigationBarTest(),
+      //child: const BottomNavigationBarTest(),
+      child: const DrawerExample(),
     ),
   );
 }
