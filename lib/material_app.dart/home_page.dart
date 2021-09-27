eee2ecc307f98c210800ce16cf22ccc89b1dc42c
Sanjib <sanjib12sinha@gmail.com>
2021-09-27 06:15:14 +0530

    material app modified x

--- lib/material_app.dart/home_page.dart
+++ lib/material_app.dart/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First Page'),
+        title: const Text('Home Page'),
       ),
       body: GestureDetector(
         onTap: () {
