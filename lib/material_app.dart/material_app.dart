aa1383e9f686dbe92cdff28782b7e543929d61e2
Sanjib <sanjib12sinha@gmail.com>
2021-09-27 05:47:25 +0530

    drawer modified

---
+++ lib/material_app.dart/material_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MaterialAppExample extends StatelessWidget {
+  const MaterialAppExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
