1933bb1860049a79576c2683708ca3eaaab268fb
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-10-06 06:58:02 +0530

    text and outlined button added

---
+++ lib/text_buttons_example/outlined_button_example.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+/// This is the main application widget.
+class OutlinedButtonExample extends StatelessWidget {
+  const OutlinedButtonExample({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const Center(
+          child: OutlinedButtonHome(),
+        ),
+      ),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class OutlinedButtonHome extends StatelessWidget {
+  const OutlinedButtonHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text(
+              'Click Me',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    alignment: Alignment.center,
+                    width: 350.00,
+                    height: 350.00,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      border: Border.all(
+                        color: Colors.red,
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                      ),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(40.0)),
+                      gradient: const LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Colors.blue,
+                          Colors.red,
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Click Me',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
