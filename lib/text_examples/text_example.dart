9031e95fa2fc3b0a74d6955e9af40a0a0fb48279
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-10-09 06:06:54 +0530

    text examples added

--- lib/text_examples/text_example.dart
+++ lib/text_examples/text_example.dart
@@ -22,7 +22,74 @@ class TextExampleHome extends StatelessWidget {
         title: Text('Text Examples in Flutter'),
       ),
       body: ListView(
-        children: [],
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Hello Text Controller',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+                color: Colors.greenAccent,
+                decoration: TextDecoration.combine(
+                  [
+                    TextDecoration.underline,
+                    TextDecoration.overline,
+                  ],
+                ),
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.wavy,
+                letterSpacing: 5.0,
+                wordSpacing: 10.0,
+                shadows: [
+                  Shadow(
+                    color: Colors.black,
+                    blurRadius: 2.0,
+                    offset: Offset(5, 1),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'More Style',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.normal,
+                  foreground: Paint()
+                    ..color = Colors.red
+                    ..strokeWidth = 2.0
+                    ..style = PaintingStyle.stroke),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'I am going to write some gibberish! What does that mean? Actually nothing! '
+              'Now you may ask should an adjective always align with the noun? Or, an '
+              'adverb should follow a verb like its shadow?',
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.normal,
+                  foreground: Paint()
+                    ..color = Colors.blue
+                    ..strokeWidth = 1.0
+                    ..style = PaintingStyle.stroke),
+              textAlign: TextAlign.left,
+              maxLines: 2,
+              // it means some more text are there waiting for you to be read
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
       ),
     );
   }
