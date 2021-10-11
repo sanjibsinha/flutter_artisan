7ef6c8687ac7289ce7e50a97ba9782e3f8a30b80
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-10-11 06:09:34 +0530

    pushing rich text

--- lib/text_examples/text_rich_example.dart
+++ lib/text_examples/text_rich_example.dart
@@ -21,42 +21,73 @@ class TextRichHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('Text rich example'),
       ),
-      body: Center(
-        child: Text.rich(
-          TextSpan(
-            text: 'Flutter',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent,
-            ), // default text style
-            children: <TextSpan>[
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: <Widget>[
+            Text.rich(
               TextSpan(
-                text: ' is',
+                text: 'Flutter',
                 style: TextStyle(
                   fontSize: 40,
-
-                  /// if we don't declare color explicitly,
-                  /// it will take default color blue
-                  color: Colors.black38,
-
-                  /// if we use the same fontFamily, that will also
-                  /// make it italic, bold and blue
-                  //fontFamily: 'Allison',
-                ),
-              ),
-              TextSpan(
-                text: ' beautiful ',
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
                   fontWeight: FontWeight.bold,
-                  fontFamily: 'Allison',
-                  fontSize: 60,
-                  color: Colors.redAccent,
-                ),
+                  color: Colors.blueAccent,
+                ), // default text style
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' is',
+                    style: TextStyle(
+                      fontSize: 40,
+
+                      /// if we don't declare color explicitly,
+                      /// it will take default color blue
+                      color: Colors.black38,
+
+                      /// if we use the same fontFamily, that will also
+                      /// make it italic, bold and blue
+                      //fontFamily: 'Allison',
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' beautiful ',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Allison',
+                      fontSize: 60,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text.rich(
+              TextSpan(
+                text: 'My name is ',
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: <InlineSpan>[
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.baseline,
+                    baseline: TextBaseline.alphabetic,
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 100),
+                      child: const TextField(),
+                    ),
+                  ),
+                  const TextSpan(
+                    text: '.',
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
