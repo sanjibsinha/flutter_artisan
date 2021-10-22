2bf4466c945ba9a9918bd1dddaf729a80ad44f1a
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-10-22 17:02:21 +0530

    sliver fixed extent list

---
+++ lib/sliver_examples/custom_delegate.dart
@@ -0,0 +1,11 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomChildDelegate extends SliverChildListDelegate {
+  CustomChildDelegate(List<Widget> children) : super(children);
+  @override
+  // TODO: implement children
+  List<Widget> get children => [];
+}
---
+++ lib/sliver_examples/sliver_fixed_extent_list_sample.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'custom_delegate.dart';
+
+class SliverFixedExtentListHome extends StatelessWidget {
+  const SliverFixedExtentListHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'SliverFixedExtentList Sample',
+      home: SliverFixedExtentListSample(),
+    );
+  }
+}
+
+class SliverFixedExtentListSample extends StatelessWidget {
+  const SliverFixedExtentListSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          title: const Text(
+            'SliverPersistentHeader Sample',
+            style: TextStyle(
+              fontFamily: 'Allison',
+              fontSize: 60,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.network(
+              'https://cdn.pixabay.com/photo/2016/09/10/17/18/book-1659717_960_720.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: CustomChildDelegate([
+            Container(color: Colors.red),
+            Container(color: Colors.green),
+            Container(color: Colors.blue),
+          ]),
+          itemExtent: 50,
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Container(
+                height: 50,
+                alignment: Alignment.center,
+                color: Colors.orange[100 * (index % 9)],
+                child: Text('orange $index'),
+              );
+            },
+            childCount: 9,
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            color: Colors.yellow,
+            padding: const EdgeInsets.all(8.0),
+            child: const Text('Grid Header', style: TextStyle(fontSize: 24)),
+          ),
+        ),
+        SliverGrid.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          childAspectRatio: 4.0,
+          children: <Widget>[
+            Container(color: Colors.red),
+            Container(color: Colors.green),
+            Container(color: Colors.blue),
+            Container(color: Colors.red),
+            Container(color: Colors.green),
+            Container(color: Colors.blue),
+          ],
+        ),
+        SliverGrid.extent(
+          maxCrossAxisExtent: 200,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          childAspectRatio: 4.0,
+          children: <Widget>[
+            Container(color: Colors.pink),
+            Container(color: Colors.indigo),
+            Container(color: Colors.orange),
+            Container(color: Colors.pink),
+            Container(color: Colors.indigo),
+            Container(color: Colors.orange),
+          ],
+        ),
+      ],
+    );
+  }
+}
