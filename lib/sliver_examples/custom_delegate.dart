6ad9955872077ee6db4c86597f5787218df8a6c4
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-10-22 20:47:09 +0530

    sliver fixed extent list and others more

--- lib/sliver_examples/custom_delegate.dart
+++ lib/sliver_examples/custom_delegate.dart
@@ -4,6 +4,5 @@ import 'package:flutter/material.dart';
 class CustomChildDelegate extends SliverChildListDelegate {
   CustomChildDelegate(List<Widget> children) : super(children);
   @override
-  // TODO: implement children
   List<Widget> get children => [];
 }
