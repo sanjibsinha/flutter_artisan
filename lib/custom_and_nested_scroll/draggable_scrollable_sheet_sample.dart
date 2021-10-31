b8fd551a2b6aee7112dd4b152708e5f60466c3e4
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-10-31 09:35:30 +0530

    modifying draggable scrollable sheet

--- lib/custom_and_nested_scroll/draggable_scrollable_sheet_sample.dart
+++ lib/custom_and_nested_scroll/draggable_scrollable_sheet_sample.dart
@@ -29,33 +29,68 @@ class _DraggableScrollableSheetHomeState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Draggable Scrollable Sheet Sample'),
+        title: const Text('Vocabulary Practice'),
       ),
       body: Stack(
         children: <Widget>[
           Image.network(
-            'https://cdn.pixabay.com/photo/2016/09/10/17/18/book-1659717_960_720.jpg',
+            'https://cdn.pixabay.com/photo/2018/04/07/08/28/notepad-3297994_960_720.jpg',
             fit: BoxFit.cover,
             width: double.infinity,
           ),
-          TextField(
-            controller: textController,
-            onSubmitted: (text) {
-              listOfItems.add(text);
-              textController.clear();
-              setState(() {});
-            },
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              'Type Below',
+              style: TextStyle(
+                fontFamily: 'Allison',
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                foreground: Paint()
+                  ..color = Colors.red
+                  ..strokeWidth = 2.0
+                  ..style = PaintingStyle.stroke,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Container(
+            margin: const EdgeInsets.all(40),
+            padding: const EdgeInsets.all(40),
+            child: TextField(
+              controller: textController,
+              onSubmitted: (text) {
+                listOfItems.add(text);
+                textController.clear();
+                setState(() {});
+              },
+            ),
           ),
           DraggableScrollableSheet(
             builder: (BuildContext context, ScrollController scrollController) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
+                  margin: const EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
                   color: Colors.blue[100],
                   child: ListView.builder(
                     itemCount: listOfItems.length,
                     itemBuilder: (BuildContext ctxt, int index) {
-                      return Text(listOfItems[index]);
+                      return Text(
+                        listOfItems[index],
+                        style: const TextStyle(
+                          fontFamily: 'Allison',
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
                     },
                   ),
                 ),
