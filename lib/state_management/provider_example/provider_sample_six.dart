36b60373f8120225e1041b2f3b18bd4c24de4ac5
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-11-10 16:34:01 +0530

    pushing consumer and selecotrs

---
+++ lib/state_management/provider_example/provider_sample_six.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_artisan/models/counter.dart';
+
+import 'package:provider/provider.dart';
+
+class ProviderSampleSix extends StatelessWidget {
+  const ProviderSampleSix({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProviderSampleSixHome(),
+    );
+  }
+}
+
+class ProviderSampleSixHome extends StatelessWidget {
+  const ProviderSampleSixHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Home page building');
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: const [
+            FirstConsumer(),
+            SecondConsumer(),
+            SelectorFirstMethod(),
+            SizedBox(
+              height: 5,
+            ),
+            SelectorSecondMethod(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FirstConsumer extends StatelessWidget {
+  const FirstConsumer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Counter>(
+      builder: (context, firstValue, child) {
+        print('Building first value');
+        return Column(
+          children: [
+            const HumongousWidget(),
+            Container(
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                'You pressed ${firstValue.count} times',
+                style: const TextStyle(
+                  fontSize: 60,
+                  fontFamily: 'Allison',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            if (child != null) child,
+          ],
+        );
+      },
+      // Build the expensive widget here.
+      child: Container(
+        padding: const EdgeInsets.only(top: 10),
+        child: const ExpensiveWidget(),
+      ),
+    );
+  }
+}
+
+class SecondConsumer extends StatelessWidget {
+  const SecondConsumer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Counter>(
+      builder: (context, secondValue, child) {
+        print('Building second value');
+        return Column(
+          children: [
+            const HumongousWidget(),
+            Container(
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                'You pressed ${secondValue.num} times',
+                style: const TextStyle(
+                  fontSize: 60,
+                  fontFamily: 'Allison',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            if (child != null) child,
+          ],
+        );
+      },
+      // Build the expensive widget here.
+      child: Container(
+        padding: const EdgeInsets.only(top: 10),
+        child: const ExpensiveWidget(),
+      ),
+    );
+  }
+}
+
+class SelectorSecondMethod extends StatelessWidget {
+  const SelectorSecondMethod({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Counter second method building');
+    return ElevatedButton(
+      onPressed: context.select((Counter c) => c.addOne),
+      child: const Text(
+        'Increment second value',
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class SelectorFirstMethod extends StatelessWidget {
+  const SelectorFirstMethod({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Counter first method building');
+    return ElevatedButton(
+      onPressed: context.select((Counter c) => c.increment),
+      child: const Text(
+        'Increment first value',
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class HumongousWidget extends StatelessWidget {
+  const HumongousWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Homongous widget building');
+    return Center(
+      /// building another humongous widget tree
+      child: Wrap(
+        direction: Axis.vertical,
+        children: [
+          Column(
+            children: const [
+              Text(
+                'First Row of Consumer Child',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blue,
+                  fontFamily: 'Allison',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'First Row of Consumer Child',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blue,
+                  fontFamily: 'Allison',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ExpensiveWidget extends StatelessWidget {
+  const ExpensiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Expensive widget building');
+    return Center(
+      /// building another humongous widget tree
+      child: Wrap(
+        direction: Axis.vertical,
+        children: [
+          Column(
+            children: const [
+              Text(
+                'First Row inside Consumer',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blue,
+                  fontFamily: 'Allison',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'First Row inside Consumer',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blue,
+                  fontFamily: 'Allison',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
