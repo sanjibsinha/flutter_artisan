756525053de44641bc89617c83bd0a81f607bb2f
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-11-10 05:55:43 +0530

    pushing provider sample four

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_artisan/state_management/provider_example/provider_sample_three.dart';
-//import 'package:flutter_artisan/state_management/provider_example/provider_sample_two.dart';
 
 import 'package:provider/provider.dart';
 //import 'state_management/provider_example/provider_sample_one.dart';
 //import 'state_management/provider_example/provider_sample_two.dart';
-
+import 'state_management/provider_example/provider_sample_four.dart';
+//import 'state_management/provider_example/provider_sample_three.dart';
+//import 'state_management/provider_example/provider_sample_two.dart';
 import 'models/counter.dart';
 
 //import 'scrollable_widgets/reorderable_listview_builder_sample.dart';
@@ -80,7 +80,8 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => Counter()),
       ],
-      child: const ProviderSampleThree(),
+      child: const ProviderSampleFour(),
+      //child: const ProviderSampleThree(),
       //child: const ProviderSampleTwo(),
     ),
   );
