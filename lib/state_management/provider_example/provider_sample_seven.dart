511de6d86cfb805fc13b8ac2bd94b36a4892d0b7
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-11-13 14:45:36 +0530

    pushing pubspec

---
+++ lib/state_management/provider_example/provider_sample_seven.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProviderSampleSeven extends StatelessWidget {
+  const ProviderSampleSeven({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProviderSampleSevenHome(),
+    );
+  }
+}
+
+class ProviderSampleSevenHome extends StatelessWidget {
+  const ProviderSampleSevenHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
