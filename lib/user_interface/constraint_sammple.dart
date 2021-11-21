bb5af0927c45466fa4b3b5bc270ca0dee4962fb4
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-11-21 08:11:55 +0530

    pushing constraint again

--- lib/user_interface/constraint_sammple.dart
+++ lib/user_interface/constraint_sammple.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ConstraintSample extends StatelessWidget {
@@ -7,6 +9,7 @@ class ConstraintSample extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Constraint Sample',
+      debugShowCheckedModeBanner: false,
       home: ConstraintSampleHomme(),
     );
   }
@@ -19,6 +22,66 @@ class ConstraintSampleHomme extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        width: 300,
+        height: 100,
+        color: Colors.amber,
+        //alignment: Alignment.bottomCenter,
+        child: Container(
+          width: 200,
+          height: 50,
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: const Text(
+            'Constraint Sample',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/**
+ * @override
+  Widget build(BuildContext context) {    
+    return Container(
+      width: 150,
+      height: 200,      
+      child: const Text('Constraint Sample'),      
+    );
+  }
+ */
+
+/**
+ *  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300,
+        height: 100,
+        color: Colors.amber,
+        alignment: Alignment.bottomCenter,        
+        child: const Text(
+          'Constraint Sample',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        
+      ),
+    );
+  }
+ */
+
+/**
+ * Center(
+      child: Container(
         width: 150,
         height: 200,
         child: Container(
@@ -28,5 +91,4 @@ class ConstraintSampleHomme extends StatelessWidget {
         ),
       ),
     );
-  }
-}
+ */
