64db6d103fa49adebdfcf0755cf9b1b21fcf91bb
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-03 09:47:43 +0530

    first branch modified

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MediaQueryHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Explore',
+      title: 'MediaQuery Sample',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
@@ -26,73 +26,298 @@ class MediaQuerySample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size(screenSize.width, 1000),
-        child: Container(
-          color: Colors.blue,
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                const Text('EXPLORE'),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: const Text(
-                          'Discover',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                      SizedBox(width: screenSize.width / 20),
-                      InkWell(
-                        onTap: () {},
-                        child: const Text(
-                          'Contact Us',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                SizedBox(
-                  width: screenSize.width / 50,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+    final appBar = AppBar(
+      title: const Text(
+        'Media Query example',
       ),
-      body: Container(
-        margin: const EdgeInsets.all(5),
-        padding: const EdgeInsets.all(5),
-        child: const Text(
-          'MediaQuery Sample',
-          style: TextStyle(
-            fontFamily: 'Allison',
-            fontSize: 70,
-            fontWeight: FontWeight.bold,
-          ),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () {},
         ),
+      ],
+    );
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return Scaffold(
+      appBar: AppBar(
+        title: appBar,
+      ),
+      body: SingleChildScrollView(
+        child: isPortrait
+            ? Column(
+// mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height -
+                            appBar.preferredSize.height -
+                            MediaQuery.of(context).padding.top) *
+                        0.5,
+                    child: ListView(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(15),
+                          alignment: Alignment.topCenter,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            border: Border.all(
+                              width: 5,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Card(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    const ListTile(
+                                      leading: Icon(Icons.album),
+                                      title: Text('Books'),
+                                      subtitle: Text('25.00'),
+                                    ),
+                                    const ListTile(
+                                      leading: Icon(Icons.album),
+                                      title: Text('Groceries'),
+                                      subtitle: Text('250.00'),
+                                    ),
+                                    const ListTile(
+                                      leading: Icon(Icons.album),
+                                      title: Text('Fruits'),
+                                      subtitle: Text('480.00'),
+                                    ),
+                                    Wrap(
+                                      alignment: WrapAlignment.spaceBetween,
+                                      direction: Axis.horizontal,
+                                      children: <Widget>[
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' ADD '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' UPDATE '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' DELETE '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' NEXT PAGE '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height -
+                            appBar.preferredSize.height -
+                            MediaQuery.of(context).padding.top) *
+                        0.5,
+                    child: ListView(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(15),
+                          alignment: Alignment.topCenter,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            border: Border.all(
+                              width: 5,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Card(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    const ListTile(
+                                      leading: Icon(Icons.album),
+                                      title: Text('Books'),
+                                      subtitle: Text('25.00'),
+                                    ),
+                                    const ListTile(
+                                      leading: Icon(Icons.album),
+                                      title: Text('Groceries'),
+                                      subtitle: Text('250.00'),
+                                    ),
+                                    const ListTile(
+                                      leading: Icon(Icons.album),
+                                      title: Text('Fruits'),
+                                      subtitle: Text('480.00'),
+                                    ),
+                                    Wrap(
+                                      alignment: WrapAlignment.spaceBetween,
+                                      direction: Axis.horizontal,
+                                      children: <Widget>[
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' ADD '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' UPDATE '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' DELETE '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.all(8),
+                                          child: ElevatedButton(
+                                            onPressed: () => {},
+                                            child: const Text(' NEXT PAGE '),
+                                            style: ElevatedButton.styleFrom(
+                                              primary: Colors.purple,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 20),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            : Container(
+                margin: const EdgeInsets.all(8),
+                child: ElevatedButton(
+                  onPressed: () => {},
+                  child: const Text(' Landscape Mode '),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.purple,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    textStyle: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => {},
       ),
     );
   }
