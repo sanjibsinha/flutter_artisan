f7140e9735ae153ba68507bdf87eea70329c981a
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-03 08:51:05 +0530

    first branch added and modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MediaQuerySample(),
+    const MediaQueryHome(),
   );
+}
+
+class MediaQueryHome extends StatelessWidget {
+  const MediaQueryHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Explore',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MediaQuerySample(),
+    );
+  }
 }
 
 class MediaQuerySample extends StatelessWidget {
@@ -11,9 +26,74 @@ class MediaQuerySample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: Container(
+          color: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                const Text('EXPLORE'),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          'Discover',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      SizedBox(width: screenSize.width / 20),
+                      InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          'Contact Us',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width / 50,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(5),
+        padding: const EdgeInsets.all(5),
+        child: const Text(
+          'MediaQuery Sample',
+          style: TextStyle(
+            fontFamily: 'Allison',
+            fontSize: 70,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
