40b8f88e6552cc943c3e15ac312e811bad93b791
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-11 13:17:37 +0530

    first commit to material app

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,168 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const OurFirstApp(),
+    const OurSecondApp(),
   );
 }
 
-class OurFirstApp extends StatelessWidget {
-  const OurFirstApp({Key? key}) : super(key: key);
+class OurSecondApp extends StatelessWidget {
+  const OurSecondApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.all(5),
-        child: const Text(
-          'Wlcome to our first flutter application',
-          textDirection: TextDirection.ltr,
-        ),
+    return const MaterialApp(
+      title: 'First Material App',
+      debugShowCheckedModeBanner: false,
+      home: OurSecondAppHome(),
+    );
+  }
+}
+
+class OurSecondAppHome extends StatelessWidget {
+  const OurSecondAppHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Material App'),
       ),
+      body: const OurSecondAppBody(),
+    );
+  }
+}
+
+class OurSecondAppBody extends StatelessWidget {
+  const OurSecondAppBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    /// Here we're going to use basic widgets
+    /// such as, Text, Container, Row, Column, etc.
+    ///
+    return Column(
+      children: [
+        Center(
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'ID',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Name',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              const Expanded(
+                child: Text(
+                  'Phone Number',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Gender',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Country',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Center(
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  '1',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'John',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              const Expanded(
+                child: Text(
+                  '123645',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Male',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Germany',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Center(
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  '2',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Jenifer',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              const Expanded(
+                child: Text(
+                  '652341',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Female',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'France',
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
