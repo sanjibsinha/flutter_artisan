871d68a55eb30679f4f51fa9d71159e4f0b0e281
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-15 06:01:41 +0530

    adding new branch basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,163 +1,179 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    const OurThirdApp(),
-  );
-}
+void main() => runApp(const MyApp());
 
-class OurThirdApp extends StatelessWidget {
-  const OurThirdApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //var size = MediaQuery.of(context).size;
     return const MaterialApp(
-      title: 'First Material App',
-      debugShowCheckedModeBanner: false,
-      home: OurThirdAppHome(),
+      home: DashBoard(
+          // size: size,
+          ),
     );
   }
 }
 
-class OurThirdAppHome extends StatelessWidget {
-  const OurThirdAppHome({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Material App'),
-      ),
-      body: const OurThirdAppBody(),
-    );
-  }
-}
-
-class OurThirdAppBody extends StatelessWidget {
-  const OurThirdAppBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    /// Here we're going to use basic widgets
-    /// such as, Text, Container, Row, Column, etc.
-    ///
-    return Column(
-      children: const [
-        ColumnOne(),
-        ColumnThree(),
-        ColumnTwo(),
-      ],
-    );
-  }
-}
-
-class ModelContainer extends StatelessWidget {
-  const ModelContainer({
+class DashBoard extends StatelessWidget {
+  //final Size size;
+  const DashBoard({
     Key? key,
-    required this.modelText,
-  }) : super(key: key);
-
-  final Text modelText;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      color: Colors.amber,
-      child: modelText,
-    );
-  }
-}
-
-class ColumnOne extends StatelessWidget {
-  const ColumnOne({
-    Key? key,
+    //required this.size,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: const [
-          ModelContainer(
-            modelText: Text('ID'),
+    var size = MediaQuery.of(context).size;
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          leading: const Icon(Icons.menu),
+          title: const Text(
+            "Dashboard",
+            textAlign: TextAlign.center,
           ),
-          ModelContainer(
-            modelText: Text('Name'),
-          ),
-          ModelContainer(
-            modelText: Text('Phone'),
-          ),
-          ModelContainer(
-            modelText: Text('Gender'),
-          ),
-          ModelContainer(
-            modelText: Text('Country'),
-          ),
-        ],
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: CustomPaint(
+                painter: ShapesPainter(),
+                child: Container(
+                  height: size.height / 2,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    createGridItem(0),
+                    createGridItem(1),
+                    createGridItem(2),
+                    createGridItem(3),
+                    createGridItem(4),
+                    createGridItem(5),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget createGridItem(int position) {
+    Color? color = Colors.white;
+    var icondata = Icons.add;
+
+    switch (position) {
+      case 0:
+        color = Colors.cyan;
+        icondata = Icons.map;
+        break;
+      case 1:
+        color = Colors.deepPurple;
+
+        icondata = Icons.add_location;
+        break;
+      case 2:
+        color = Colors.orange[300];
+        icondata = Icons.library_books;
+        break;
+      case 3:
+        color = Colors.pinkAccent;
+        icondata = Icons.access_time;
+        break;
+      case 4:
+        color = Colors.teal[900];
+        icondata = Icons.add_shopping_cart;
+        break;
+      case 5:
+        color = Colors.green[600];
+        icondata = Icons.list;
+        break;
+    }
+
+    return Builder(builder: (context) {
+      return Padding(
+        padding:
+            const EdgeInsets.only(left: 10.0, right: 10, bottom: 5, top: 5),
+        child: Card(
+          elevation: 10,
+          color: color,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            side: BorderSide(color: Colors.white),
+          ),
+          child: InkWell(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Selected Item $position")));
+            },
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Icon(
+                    icondata,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Add",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
 
-class ColumnTwo extends StatelessWidget {
-  const ColumnTwo({
-    Key? key,
-  }) : super(key: key);
+class ShapesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    // set the paint color to be white
+    paint.color = Colors.white;
+    // Create a rectangle with size and width same as the canvas
+    var rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    // draw the rectangle using the paint
+    canvas.drawRect(rect, paint);
+    paint.color = Colors.greenAccent;
+    // create a path
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, 0);
+    // close the path to form a bounded shape
+    path.close();
+    canvas.drawPath(path, paint);
+    // set the color property of the paint
+    paint.color = Colors.deepOrange;
+    // center of the canvas is (x,y) => (width/2, height/2)
+    var center = Offset(size.width / 2, size.height / 2);
+    // draw the circle with center having radius 75.0
+    canvas.drawCircle(center, 75.0, paint);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: const [
-          ModelContainer(
-            modelText: Text('2'),
-          ),
-          ModelContainer(
-            modelText: Text('Juliet'),
-          ),
-          ModelContainer(
-            modelText: Text('100023'),
-          ),
-          ModelContainer(
-            modelText: Text('Female'),
-          ),
-          ModelContainer(
-            modelText: Text('Britain'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ColumnThree extends StatelessWidget {
-  const ColumnThree({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: const [
-          ModelContainer(
-            modelText: Text('1'),
-          ),
-          ModelContainer(
-            modelText: Text('Romeo'),
-          ),
-          ModelContainer(
-            modelText: Text('489023'),
-          ),
-          ModelContainer(
-            modelText: Text('Male'),
-          ),
-          ModelContainer(
-            modelText: Text('France'),
-          ),
-        ],
-      ),
-    );
-  }
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
