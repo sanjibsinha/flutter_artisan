7d1773d5730cb4e7513022bbbb9f47d4b2c92aa6
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-12-21 06:56:16 +0530

    first again

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_artisan/main.dart';
+
+import 'package:flutter_artisan/view/my_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
