1426a537ca8bfb75cfbcb5d2395fc397833ed5f6
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-07 19:55:31 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'view/my_app.dart';
+import 'view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(title: 'Animations demo'),
+    );
+  }
 }
