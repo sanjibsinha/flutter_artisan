1d0fc99be74e984e0cd2e32b5c2fb8721ce0c48f
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-20 18:37:43 +0530

    first push final again

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ class MyStatefulWidget extends StatefulWidget {
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   double targetValue = 24.0;
-  bool selected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,64 +37,20 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       tween: Tween<double>(begin: 0, end: targetValue),
       duration: const Duration(seconds: 1),
       builder: (BuildContext context, double size, Widget? child) {
-        return GestureDetector(
-          onTap: () {
+        return TextButton(
+          onPressed: () {
             setState(() {
               targetValue = targetValue == 24.0 ? 48.0 : 24.0;
-              selected = !selected;
             });
           },
-          child: child!,
+          child: Text(
+            'Tap Me',
+            style: TextStyle(
+              fontSize: size,
+            ),
+          ),
         );
       },
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            MyAnimatedContainer(
-              selected: selected,
-              newDuration: const Duration(seconds: 2),
-              src:
-                  'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_960_720.jpg',
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            MyAnimatedContainer(
-              selected: selected,
-              newDuration: const Duration(seconds: 10),
-              src:
-                  'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_960_720.jpg',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyAnimatedContainer extends StatelessWidget {
-  const MyAnimatedContainer({
-    Key? key,
-    required this.selected,
-    required this.newDuration,
-    required this.src,
-  }) : super(key: key);
-
-  final bool selected;
-  final Duration newDuration;
-  final String src;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      width: selected ? 200.0 : 100.0,
-      height: selected ? 50.0 : 100.0,
-      color: selected ? Colors.indigoAccent : Colors.yellowAccent,
-      alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
-      duration: newDuration,
-      curve: Curves.fastOutSlowIn,
-      child: Image.network(src),
     );
   }
 }
