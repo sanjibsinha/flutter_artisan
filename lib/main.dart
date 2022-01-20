3065df083d61801ffa39c9a513aea1509f11f691
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-20 18:24:29 +0530

    first push final

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,101 @@
 import 'package:flutter/material.dart';
 
-import 'view/my_app.dart';
-
 void main() => runApp(const MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  static const String _title = 'Flutter Code Sample';
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
+  }
+}
 
-
-
-
-/* class MyStatefulWidget extends StatefulWidget {
+class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
-class _MyStatefulWidgetState extends State<MyStatefulWidget>
-    with TickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 10),
-    vsync: this,
-  )..repeat();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  double targetValue = 24.0;
+  bool selected = false;
 
   @override
   Widget build(BuildContext context) {
-    return RotatingWheel(controller: _controller);
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0, end: targetValue),
+      duration: const Duration(seconds: 1),
+      builder: (BuildContext context, double size, Widget? child) {
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              targetValue = targetValue == 24.0 ? 48.0 : 24.0;
+              selected = !selected;
+            });
+          },
+          child: child!,
+        );
+      },
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            MyAnimatedContainer(
+              selected: selected,
+              newDuration: const Duration(seconds: 2),
+              src:
+                  'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_960_720.jpg',
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            MyAnimatedContainer(
+              selected: selected,
+              newDuration: const Duration(seconds: 10),
+              src:
+                  'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_960_720.jpg',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
- */
+
+class MyAnimatedContainer extends StatelessWidget {
+  const MyAnimatedContainer({
+    Key? key,
+    required this.selected,
+    required this.newDuration,
+    required this.src,
+  }) : super(key: key);
+
+  final bool selected;
+  final Duration newDuration;
+  final String src;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      width: selected ? 200.0 : 100.0,
+      height: selected ? 50.0 : 100.0,
+      color: selected ? Colors.indigoAccent : Colors.yellowAccent,
+      alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
+      duration: newDuration,
+      curve: Curves.fastOutSlowIn,
+      child: Image.network(src),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-
-import 'package:flutter_artisan/view/my_app.dart';
+import 'package:flutter_artisan/main.dart';
 
 import 'package:flutter_test/flutter_test.dart';
 
