8112d0bc36dcb2053d593fb92f982251cb18d656
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-01-28 16:25:18 +0530

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -32,42 +32,56 @@ class MyApp extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
+        body: const RotationTransitionExample(),
       ),
     );
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
+class RotationTransitionExample extends StatefulWidget {
+  const RotationTransitionExample({Key? key}) : super(key: key);
+
+  @override
+  State<RotationTransitionExample> createState() =>
+      _RotationTransitionExampleState();
+}
+
+class _RotationTransitionExampleState extends State<RotationTransitionExample>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 20),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+    reverseCurve: Curves.easeOut,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ChangeRotation turns = Provider.of<ChangeRotation>(context);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-          child: const Text('turns Image'),
-          onPressed: turns.changeRotation,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(50),
-          child: AnimatedRotation(
-            turns: turns.turns,
-            duration: const Duration(seconds: 1),
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                width: 250,
-                height: 250,
-                child: Image.network(
-                    'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_960_720.jpg'),
-              ),
+    return Scaffold(
+      body: RotationTransition(
+        turns: _animation,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              width: 250,
+              height: 250,
+              child: Image.network(
+                  'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_960_720.jpg'),
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
