83e36bfe663739f7902554d7a546aba9909f98dd
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-26 17:58:39 +0530

    experimenting

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
+import 'view/count_down_timer_app.dart';
 
-import 'view/show_cirular_count_down_timer.dart';
+void main() => runApp(const CountDownTimerApp());
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const ShowCircularCountDownTimer(),
-      theme: ThemeData.dark(),
-    );
-  }
-}
+// flutter run -d chrome --web-renderer html
---
+++ lib/view/count_down_timer_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'show_cirular_count_down_timer.dart';
+
+class CountDownTimerApp extends StatelessWidget {
+  const CountDownTimerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const ShowCircularCountDownTimer(),
+      theme: ThemeData.dark(),
+    );
+  }
+}
